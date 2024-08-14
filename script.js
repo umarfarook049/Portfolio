0db82afc6986d0d10a9be64a9bf9dd9b233e37d3
umar <shaikumarfarook970@gmail.com>
2024-08-14 14:35:37 +0530

    remove the menu iconin the dektop

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('show');
+    });
+  });
+</script>
